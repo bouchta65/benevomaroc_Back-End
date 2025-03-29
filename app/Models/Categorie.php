990249e55b29d55eef918b143c9a7fd753368cfb
bouchta65 <bouchtamohamed01@gmail.com>
2025-03-29 00:53:47 +0000

    Ajout du modèle Categorie avec ses relations

---
+++ app/Models/Categorie.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Categorie extends Model
+{
+    protected $fillable = [
+        'nom' , 'description'
+    ];
+
+    public function evenements()
+    {
+        return $this->hasMany(Evenement::class);   
+    }
+}
