d3c8dd8c0b06919d8c0d5f305ee1378fc0bbafff
bouchta65 <bouchtamohamed01@gmail.com>
2025-03-31 20:37:45 +0000

    Ajout de toutes les tables du projet

---
+++ database/migrations/2025_03_31_201245_create_benevoles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('benevoles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->string('domaines_action');
+            $table->string('types_mission');
+            $table->string('disponibilites');
+            $table->text('missions_preferrees');
+            $table->text('talents');
+            $table->string('niveau_etudes');
+            $table->string('metier')->nullable();
+            $table->string('cv')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('benevoles');
+    }
+};
---
+++ database/migrations/2025_03_31_201351_create_postules_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('postules', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('benevole_id')->constrained('benevoles')->onDelete('cascade');
+            $table->foreignId('evenement_id')->constrained('evenements')->onDelete('cascade');
+            $table->string('etat');  
+            $table->date('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('postules');
+    }
+};
