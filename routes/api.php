8270d339cc710185ca8f4f1962f078b6376e0ffa
bouchta65 <bouchtamohamed01@gmail.com>
2025-04-02 03:14:07 +0000

    ajout des routes pour l'authentification et la gestion des bénévoles et associations

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+Use App\http\Controllers\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout',[AuthController::class,'logout']);
+    Route::put('/benevole', [AuthController::class, 'updateBenevole']);
+    Route::put('/association', [AuthController::class, 'updateAssociation']);
+});
+
+
+Route::post('login',[AuthController::class,'login']);
+Route::post('/benevole',[AuthController::class,'registerBenevole']);
+Route::get('benevolles',[AuthController::class,'getAllBenevoles']);
+Route::post('/association',[AuthController::class,'registerAssociation']);
