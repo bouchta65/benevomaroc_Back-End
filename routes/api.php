1bce2da1156d8f35ef05232256dce294776c9142
bouchta65 <bouchtamohamed01@gmail.com>
2025-04-04 02:11:13 +0000

    Ajout de la vérification de rôle pour les routes : association et bénévole

--- routes/api.php
+++ routes/api.php
@@ -3,15 +3,27 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 Use App\http\Controllers\AuthController;
+Use App\http\Controllers\EventController;
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout',[AuthController::class,'logout']);
-    Route::put('/benevole', [AuthController::class, 'updateBenevole']);
-    Route::put('/association', [AuthController::class, 'updateAssociation']);
 });
+
+Route::middleware(['auth:sanctum', 'role:association'])->group(function () {
+    Route::put('/association', [AuthController::class, 'updateAssociation']);
+    Route::post('/Evenement',[EventController::class,'addEvent']);
+    Route::put('/Evenement/{id}',[EventController::class,'updateEvent']);
+    Route::delete('/Evenement/{id}',[EventController::class,'deleteEvent']);
+});
+
+Route::middleware(['auth:sanctum', 'role:benevole'])->group(function () {
+    Route::put('/benevole', [AuthController::class, 'updateBenevole']);
+});
+
+
+
 
 
 Route::post('login',[AuthController::class,'login']);
 Route::post('/benevole',[AuthController::class,'registerBenevole']);
-Route::get('benevolles',[AuthController::class,'getAllBenevoles']);
 Route::post('/association',[AuthController::class,'registerAssociation']);
