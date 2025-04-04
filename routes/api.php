8f9c1cc8ca3019e3bc5a8a766b08bedea3832b4a
bouchta65 <bouchtamohamed01@gmail.com>
2025-04-04 03:04:25 +0000

    Ajout des routes pour la gestion des catégories (ajout, mise à jour, suppression) avec rôle admin.

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 Use App\http\Controllers\AuthController;
+use App\Http\Controllers\CategorieController;
 Use App\http\Controllers\EventController;
 
 Route::middleware('auth:sanctum')->group(function () {
@@ -11,14 +12,21 @@ Route::middleware('auth:sanctum')->group(function () {
 
 Route::middleware(['auth:sanctum', 'role:association'])->group(function () {
     Route::put('/association', [AuthController::class, 'updateAssociation']);
-    Route::post('/Evenement',[EventController::class,'addEvent']);
-    Route::put('/Evenement/{id}',[EventController::class,'updateEvent']);
-    Route::delete('/Evenement/{id}',[EventController::class,'deleteEvent']);
+    Route::post('/evenement',[EventController::class,'addEvent']);
+    Route::put('/evenement/{id}',[EventController::class,'updateEvent']);
+    Route::delete('/evenement/{id}',[EventController::class,'deleteEvent']);
 });
 
 Route::middleware(['auth:sanctum', 'role:benevole'])->group(function () {
     Route::put('/benevole', [AuthController::class, 'updateBenevole']);
 });
+
+Route::middleware(['auth:sanctum', 'role:admin'])->group(function () {
+    Route::post('/categorie', [CategorieController::class, 'addCategorie']);
+    Route::put('/categorie/{id}', [CategorieController::class, 'updateCategorie']);
+    Route::delete('/categorie/{id}', [CategorieController::class, 'deleteCategorie']);
+});
+
 
 
 
