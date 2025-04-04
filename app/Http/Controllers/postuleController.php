24bfdc86e3375658079e095c86a7f0d28389ac56
bouchta65 <bouchtamohamed01@gmail.com>
2025-04-04 23:48:55 +0000

    Ajout de la méthode postulationByEvent pour récupérer les postulations liées à un événement

--- app/Http/Controllers/postuleController.php
+++ app/Http/Controllers/postuleController.php
@@ -18,5 +18,35 @@ class postuleController extends Controller
         return $postuled ? true : false;
     }
 
-   
+    public function addPostulation($event_id)
+    {
+        try {
+
+            if($this->hasAlreadyPostulated($event_id)){
+                return response()->json(["message" => "Vous avez déjà postulé pour cet événement"], 400);
+            }
+            $benevole_id = Benevole::where('user_id',Auth::user()->id)->first();
+
+            $postulation = Postule::create([
+                'benevole_id' => $benevole_id->id,
+                'evenement_id' => $event_id,
+                'etat' => 'en attent', 
+                'date' => Carbon::now(), 
+            ]);
+
+            return response()->json(["message" => "Postulation ajoutée avec succès", "postulation" => $postulation], 201);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Erreur lors de l\'ajout de la postulation', 'error' => $e->getMessage()], 500);
+        }
+    }
+
+    public function postulationByEvent($event_id){
+        try{
+            $postulation = Postule::where('evenement_id',$event_id)->get();
+            return response()->json(["message" => "Postulations récupérées avec succès. ", "postulation" => $postulation], 201);
+
+        }catch(\Exception $e){
+            return response()->json(['message' => 'Erreur lors de l\'ajout de la postulation', 'error' => $e->getMessage()], 500);
+        }
+    }
 }
