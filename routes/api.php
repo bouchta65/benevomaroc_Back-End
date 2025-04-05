b4665fffccc8adfc5270386c9b2444b6e5146092
bouchta65 <bouchtamohamed01@gmail.com>
2025-04-05 03:55:45 +0000

    Ajout des routes pour la gestion des postulations et des événements

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 Use App\http\Controllers\AuthController;
 use App\Http\Controllers\CategorieController;
 Use App\http\Controllers\EventController;
+use App\Http\Controllers\postuleController;
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout',[AuthController::class,'logout']);
@@ -17,15 +18,30 @@ Route::middleware(['auth:sanctum', 'role:association'])->group(function () {
     Route::delete('/evenement/{id}',[EventController::class,'deleteEvent']);
 });
 
+Route::middleware(['auth:sanctum', 'role:association','CheckEventOwner'])->group(function () {
+    Route::put('/evenement/{id}',[EventController::class,'updateEvent']);
+    Route::delete('/evenement/{id}',[EventController::class,'deleteEvent']);
+    Route::get('/evenement/{id}/postulations',[postuleController::class,'postulationByEvent']);
+    Route::put('/evenement/{id}/postulations/{benevole_id}', [PostuleController::class, 'changeStatusBnenvole']);
+});
+
 Route::middleware(['auth:sanctum', 'role:benevole'])->group(function () {
     Route::put('/benevole', [AuthController::class, 'updateBenevole']);
+    Route::post('/evenement/{id}', [postuleController::class, 'addPostulation']);
+    Route::delete('/evenement/{id}', [postuleController::class, 'cancelPostulation']);
+    Route::get('/evenement/{id}', [EventController::class, 'getEventById']);
+
 });
 
 Route::middleware(['auth:sanctum', 'role:admin'])->group(function () {
     Route::post('/categorie', [CategorieController::class, 'addCategorie']);
     Route::put('/categorie/{id}', [CategorieController::class, 'updateCategorie']);
     Route::delete('/categorie/{id}', [CategorieController::class, 'deleteCategorie']);
+    Route::get('/categorie', [CategorieController::class, 'getCategorie']);
 });
+
+
+Route::get('/evenemens', [EventController::class, 'getAllEvent']);
 
 
 
