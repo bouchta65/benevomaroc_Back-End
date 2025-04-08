72ada6aa2ab8c488d6fef9ad7fef9c63ef1850bf
bouchta65 <bouchtamohamed01@gmail.com>
2025-04-08 06:11:49 +0100

    Ajout de la méthode getProfile() pour récupérer les informations du profil de l'utilisateur en fonction de son rôle (bénévole ou association ou admin)

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -8,8 +8,11 @@ use App\Models\Benevole;
 use App\Models\Association;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Validation\Rules\Password;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Mail;
 
 class ProfileController extends Controller
 {
@@ -21,12 +24,24 @@ class ProfileController extends Controller
                 $benevole = Benevole::join('users', 'benevoles.user_id', '=', 'users.id')->where('users.id', $user->id)->select('benevoles.*', 'users.*') ->first();     
                     return response()->json(["benevole" => $benevole], 200);
             } elseif ($user->role == 'association') {
-                $association = Association::join('users','association.user_id','=','user.id')->where('user.id',$user->id)->select('association.*','benevole.*')->first();
+                $association = Association::join('users','associations.user_id','=','users.id')->where('users.id',$user->id)->select('associations.*','users.*')->first();
                 return response()->json(["association" => $association], 200);
+            } elseif ($user->role == 'admin') {
+                $admin = User::where('id', $user->id)->first();
+                return response()->json(["admin" => $admin], 200);
             }
             return response()->json(["message" => "Profil introuvable"], 404);
         } catch (\Exception $e) {
             return response()->json(["message" => "Erreur", "error" => $e->getMessage()], 500);
         }
     }
+
+    
+    
+    
+
+
+
+
+
 }
