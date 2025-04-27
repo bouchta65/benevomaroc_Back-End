6523deb67c02a72f19bf11e11d4d075afe83ea86
bouchta65 <bouchtamohamed01@gmail.com>
2025-04-27 15:30:01 +0100

    Suppression de la propriété 'type' des opportunités

--- app/Models/Opportunite.php
+++ app/Models/Opportunite.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Opportunite extends Model
 {
     protected $fillable = [
-        'titre', 'description', 'date', 'derniere_date_postule' ,'ville' , 'pays' , 'adresse' , 'association_id' , 'categorie_id' , 'image' , 'status' , 'nb_benevole' , 'duree' , 'engagement_requis','missions_principales','competences' ,'type'
+        'titre', 'description', 'date', 'derniere_date_postule' ,'ville' , 'pays' , 'adresse' , 'association_id' , 'categorie_id' , 'image' , 'status' , 'nb_benevole' , 'duree' , 'engagement_requis','missions_principales','competences'
     ];
 
     public function association()
