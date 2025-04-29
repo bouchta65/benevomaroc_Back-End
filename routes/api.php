2095541b722f5730848f02a22945c52a22f5dd3f
bouchta65 <bouchtamohamed01@gmail.com>
2025-04-30 00:12:15 +0100

    ajout de nouvelles routes dans routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -16,21 +16,25 @@ Route::middleware('auth:sanctum')->group(function () {
 Route::middleware(['auth:sanctum', 'role:association'])->group(function () {
     Route::post('/dashboard/opportunite',[OpportunitesController::class,'addOpportunite']);
     Route::get('/dashboard/profile', [ProfileController::class, 'getProfile']);
-    Route::put('/dashboard/profile/association/userInfo', [ProfileController::class, 'updateUserInfo']);
-    Route::put('/dashboard/profile/association/details', [ProfileController::class, 'updateAssociationDetails']);
+    Route::post('/dashboard/profile/association/userInfo', [ProfileController::class, 'updateUserInfo']);
+    Route::post('/dashboard/profile/association/details', [ProfileController::class, 'updateAssociationDetails']);
     Route::put('/dashboard/profile/association/password', [ProfileController::class, 'updatePassword']);
+    Route::get('/dashboard/Myopportunites', [OpportunitesController::class, 'getOpportunitesByAssociation']);
+    Route::get('/dashboard/lastThreeOpportunites', [OpportunitesController::class, 'getLastThreeOpportunitesActives']);
+    Route::get('/dashboard/opportunite/{id}/postulations',[postuleController::class,'postulationByOpportunite']);
+    Route::get('/dashboard/opportunite/postulations',[postuleController::class,'getAllPostulationsByAssociation']);
+    Route::get('/dashboard/opportunite/benevole/{id}',[ProfileController::class,'getBenevoleData']);
 
 });
 
 Route::middleware(['auth:sanctum', 'role:association','CheckOpportuniteOwner'])->group(function () {
-    Route::put('/dashboard/opportunite/{id}',[OpportunitesController::class,'updateOpportunite']);
+    Route::post('/dashboard/opportunite/{id}',[OpportunitesController::class,'updateOpportunite']);
     Route::delete('/dashboard/opportunite/{id}',[OpportunitesController::class,'deleteOpportunite']);
-    Route::get('/dashboard/opportunite/{id}/postulations',[postuleController::class,'postulationByOpportunite']);
-    Route::put('/dashboard/opportunite/{id}/postulations/{benevole_id}', [PostuleController::class, 'changeStatusBnenvole']);
+    Route::post('/dashboard/opportunite/{id}/postulations/{benevole_id}', [PostuleController::class, 'changeStatusBenevole']);
 });
 
 Route::middleware(['auth:sanctum', 'role:benevole'])->group(function () {
-    Route::post('/opportunite/{id}', [postuleController::class, 'addPostulation']);
+    Route::post('/benevole/postulation/add/{id}', [postuleController::class, 'addPostulation']);
     Route::delete('/opportunite/{id}', [postuleController::class, 'cancelPostulation']);
     Route::get('/mespostulations', [postuleController::class, 'benevolePostulation']);
     Route::get('/profile', [ProfileController::class, 'getProfile']);
@@ -39,6 +43,7 @@ Route::middleware(['auth:sanctum', 'role:benevole'])->group(function () {
     Route::put('/profile/benevole/password', [ProfileController::class, 'updatePassword']);
     Route::Post('/profile/benevole/reset-password', [ProfileController::class, 'reset']);
     Route::get('/profile/benevole/top3Opportunites', [postuleController::class, 'top3Opportunites']);
+    Route::get('/benevole/postulation/check/{id}', [postuleController::class, 'hasAlreadyPostulated']);
 
 
 });
@@ -47,9 +52,9 @@ Route::middleware(['auth:sanctum', 'role:association'])->group(function () {
     Route::post('/categorie', [CategorieController::class, 'addCategorie']);
     Route::put('/categorie/{id}', [CategorieController::class, 'updateCategorie']);
     Route::delete('/categorie/{id}', [CategorieController::class, 'deleteCategorie']);
-    Route::get('/categorie', [CategorieController::class, 'getCategorie']);
     Route::put('/dashboard/profile/admin/userInfo', [ProfileController::class, 'updateUserInfo']);
     Route::put('/dashboard/profile/admin/password', [ProfileController::class, 'updatePassword']);
+    
     
 });
 
@@ -60,6 +65,7 @@ Route::get('/opportunites/type', [OpportunitesController::class, 'filterByTypes'
 Route::get('/opportunites/populare', [OpportunitesController::class, 'getMostPopularOpportunites']);
 Route::get('/opportunites/recent', [OpportunitesController::class, 'getRecentOpportunites']);
 Route::get('/opportunites/{id}/similar', [OpportunitesController::class, 'getSimilarOpportunites']);
+Route::get('/categorie', [CategorieController::class, 'getCategorie']);
 
 
 
